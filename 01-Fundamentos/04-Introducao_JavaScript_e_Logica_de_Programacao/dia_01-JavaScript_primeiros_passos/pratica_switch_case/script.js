74eb80807678afec81d90654fc055a8ba56445c1
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-28 16:12:58 -0300

    adicionado pratica de switch/case

---
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_01-JavaScript_primeiros_passos/pratica_switch_case/script.js
@@ -0,0 +1,20 @@
+let status = "lista";
+
+switch (status) {
+  case "aprovada":
+    console.log("Parabéns! Você passou.");
+    break;
+
+  case "lista":
+    console.log("Você está em nossa lista de espera.");
+    break;
+
+  case "reprovada":
+    console.log("Você foi reprovado");
+    break;
+
+  default:
+    console.log("Não se aplica");
+}
+
+
