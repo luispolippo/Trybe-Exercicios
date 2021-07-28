323df41cedc227d6c479445d5c7f8e9d9290df62
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-28 15:28:52 -0300

    adicionado bloco 4 e primeira pratica

---
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_01-JavaScript_primeiros_passos/pratica_variaveis/script.js
@@ -0,0 +1,15 @@
+const myName = "Guilherme";
+const birthCity = "Paulista";
+let birthYear = 1994;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Canoas";
+console.log(birthCity);
+
+/* O Erro ocorre pq não é possível alterar o valor de uma contante*/
