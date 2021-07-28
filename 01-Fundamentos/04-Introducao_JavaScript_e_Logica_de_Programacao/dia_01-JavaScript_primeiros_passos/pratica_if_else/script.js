4e3ac26893ea7d3c35f755f5dbddcdbd70efb921
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-28 16:00:20 -0300

    adicionado pratica de if/else

---
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_01-JavaScript_primeiros_passos/pratica_if_else/script.js
@@ -0,0 +1,10 @@
+let nota = 85;
+
+if (nota >= 80) {
+  console.log("Parabéns! Você foi aprovado");
+} else if (nota >= 60) {
+  console.log("Você está na nossa lista de espera");
+} else {
+  console.log("Você foi reprovado");
+}
+
