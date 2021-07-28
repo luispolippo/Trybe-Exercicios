9326a715ee073bd8c5fdc57f9858c3046dcd2066
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-28 20:29:34 -0300

    adicionado exercicio seParEntaoTrue.js

---
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_01-JavaScript_primeiros_passos/exercicios/seParEntaoTrue.js
@@ -0,0 +1,12 @@
+let num1 = 35;
+let num2 = 15;
+let num3 = 7;
+let result;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
