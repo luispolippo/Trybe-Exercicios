77b4d8a0a8f03694edbd78ae9eec13744b7e4773
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-29 17:53:14 -0300

    correçao no exercicio 5

--- 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_02-JavaScript_Array_e_Loop_For/Exercicios/exercicio5.js
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_02-JavaScript_Array_e_Loop_For/Exercicios/exercicio5.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maiorValor = 0;
+let maiorValor = numbers[0];
 
 for(let number of numbers){
   if(number > maiorValor){
@@ -8,4 +8,4 @@ for(let number of numbers){
   }
 }
 
-console.log(maiorValor);
+console.log("Maior valor: " + maiorValor);
