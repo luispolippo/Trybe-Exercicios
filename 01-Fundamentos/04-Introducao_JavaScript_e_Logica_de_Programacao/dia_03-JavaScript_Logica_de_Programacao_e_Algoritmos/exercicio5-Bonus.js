d5b734df478b02ffcfadc0ecce9f1ee29d070bf1
Guilherme Polippo <luispolippo@hotmail.com>
2021-07-30 20:49:39 -0300

    adicionado exercicio 5 Bonus

--- 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_03-JavaScript_Logica_de_Programacao_e_Algoritmos/exercicio5-Bonus.js
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_03-JavaScript_Logica_de_Programacao_e_Algoritmos/exercicio5-Bonus.js
@@ -1,12 +1,12 @@
-let n = 9;
+let n = 7;
 let meioPiramide = (n + 1) / 2;
-let linha = "";
 let aEsquerda = meioPiramide;
 let aDireita = meioPiramide;
+let linha = "";
 
-for (let i = 0; i <= meioPiramide; i += 1) {
-  for (let j = 0; j <= n; j += 1) {
-    if (j > aEsquerda &&  j < aDireita) {
+for (let i = 1; i <= meioPiramide; i += 1) {
+  for (let j = 1; j <= n; j += 1) {
+    if (j === aEsquerda ||  j === aDireita || i === meioPiramide) {
       linha += "*";
     } else {
       linha += " ";
@@ -14,7 +14,6 @@ for (let i = 0; i <= meioPiramide; i += 1) {
   }
   console.log(linha);
   linha = "";
-  aDireita += 1;
   aEsquerda -= 1;
+  aDireita += 1;
 }
-
