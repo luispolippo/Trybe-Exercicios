d44994407c3e0eeb334ccd19261dc2d15e66147f
Guilherme Polippo <luispolippo@hotmail.com>
2021-08-02 18:23:39 -0300

    refatorado exercicio xadrez com funcoes

--- 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_04-JavaScript_Objetos_e_Funcoes/Para_Fixar/funcoes/xadrez.js
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_04-JavaScript_Objetos_e_Funcoes/Para_Fixar/funcoes/xadrez.js
@@ -1,30 +1,28 @@
 let piece = "RaiNha";
-let result;
-let pieceLower = piece.toLowerCase();
 
-switch (pieceLower) {
-  case "rei":
-    result = "Uma casa para qualquer direção";
-    break;
-  case "rainha":
-    result =
-      "Move-se ao longo da horizontal, vertical e diagonais, em linha reta, mas não pode pular outras peças.";
-    break;
-  case "bispo":
-    result = "diagonal mas não pode pular outras peças";
-    break;
-  case "cavalo":
-    result = "move-se em forma de L e pode pular outras peças";
-    break;
-  case "torre":
-    result = "vertical ou horizontal";
-    break;
-  case "peão":
-    result =
-      "O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.";
-    break;
-  default:
-    result = "Erro! Peça inválida";
+function chessMove(piece) {
+  switch (piece.toLowerCase()) {
+    case "rei":
+      return "Uma casa para qualquer direção";
+      break;
+    case "rainha":
+      return "Move-se ao longo da horizontal, vertical e diagonais, em linha reta, mas não pode pular outras peças.";
+      break;
+    case "bispo":
+      return "diagonal mas não pode pular outras peças";
+      break;
+    case "cavalo":
+      return "move-se em forma de L e pode pular outras peças";
+      break;
+    case "torre":
+      return "vertical ou horizontal";
+      break;
+    case "peão":
+      return "O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.";
+      break;
+    default:
+      return "Erro! Peça inválida";
+  }
 }
 
-console.log(result);
+console.log(piece + " : " + chessMove(piece));
