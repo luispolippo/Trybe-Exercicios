1838c2f4ed874e48310c14d598e1fa50f4b5e404
Guilherme Polippo <luispolippo@hotmail.com>
2021-08-02 21:26:05 -0300

    adicionado exercicio menorValorArray e consertado maiorValorArray

--- 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_04-JavaScript_Objetos_e_Funcoes/exercicios/maiorValorArray.js
+++ 01-Fundamentos/04-Introducao_JavaScript_e_Logica_de_Programacao/dia_04-JavaScript_Objetos_e_Funcoes/exercicios/maiorValorArray.js
@@ -2,13 +2,13 @@ function indexOfMaiorValor(array) {
   let maiorValor = array[0];
 
   for (let key in array) {
-    if (array[key] > maiorValor) {
+    if (array[key] > array[maiorValor]) {
       maiorValor = key;
     }
   }
   return maiorValor;
 }
 
-array = [2, 3, 6, 7, 0, 8];
+array = [2, 3, 9, 7, 0, 8];
 
 console.log(indexOfMaiorValor(array));
