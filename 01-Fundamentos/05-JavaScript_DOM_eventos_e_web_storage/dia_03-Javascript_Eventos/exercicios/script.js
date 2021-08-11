c797056b63e20e2235e9510db6fd626ad1c8193f
Guilherme Polippo <luispolippo@hotmail.com>
2021-08-11 16:16:38 -0300

    Requisito 1 do exercicio dia 5.3

--- 01-Fundamentos/05-JavaScript_DOM_eventos_e_web_storage/dia_03-Javascript_Eventos/exercicios/script.js
+++ 01-Fundamentos/05-JavaScript_DOM_eventos_e_web_storage/dia_03-Javascript_Eventos/exercicios/script.js
@@ -1,16 +1,48 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
-    const dayListItem = document.createElement('li');
+    const dayListItem = document.createElement("li");
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
+}
 
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Requisito 1
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+const daysList = document.getElementById("days");
+
+for (let i = 0; i < dezDaysList.length; i += 1) {
+  let day = dezDaysList[i];
+  let li = document.createElement("li");
+  li.innerHTML = day;
+
+  li.className = "day";
+  if (day === 24 || day === 25 || day === 31) {
+    li.className += " holiday";
+  }
+  if (day === 4 || day === 11 || day === 18 || day === 25) {
+    li.className += " friday";
+  }
+
+  daysList.appendChild(li);
+}
