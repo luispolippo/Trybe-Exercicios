46eb9015a16ffe285c6dd1cf56994265eee4b48f
Guilherme Polippo <luispolippo@hotmail.com>
2021-08-31 21:28:30 -0300

    Iniciado exercicios bonus dia 7.3

---
+++ 01-Fundamentos/07-JavaScript_ES6_e_Testes_Unitarios/dia_03-Testes_unitarios/exercicios/bonus/exercicio1.js
@@ -0,0 +1,50 @@
+function getChange(payable, paid) {
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  const change = [];
+  const { length } = coins;
+  let remaining = paid - payable;
+
+  // escreva seu código aqui...
+  if (remaining < 0) {
+    throw Error('paid value is not enough');
+  }
+
+  let count = 0;
+  
+  while(count !== remaining) {
+    for (let value of coins) {
+      count = 0;
+      for (let value of change) {
+        count += value;
+      }
+      if (remaining >= value && count !== remaining) {
+        change.push(value);
+      }
+    }
+  } 
+    
+  return change;
+}
+
+console.log(getChange(486, 600));
+
+const { throws } = require('assert');
+const assert = require('assert');
+
+let result = getChange(1, 1); // no change/coins just an empty array
+let expected = [];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(215, 300); // expect an array containing [50, 20, 10, 5]
+expected = [50, 20, 10, 5];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(486, 600); // expect an array containing [100, 10, 2, 2]
+expected = [100, 10, 2, 2];
+assert.deepStrictEqual(result, expected);
+
+result = getChange(12, 400); // expect an array containing [200, 100, 50, 20, 10, 5, 2, 1]
+expected = [200, 100, 50, 20, 10, 5, 2, 1];
+assert.deepStrictEqual(result, expected);
+
+assert.throws(() => { getChange(100, 10); }, /^Error: paid value is not enough$/);
