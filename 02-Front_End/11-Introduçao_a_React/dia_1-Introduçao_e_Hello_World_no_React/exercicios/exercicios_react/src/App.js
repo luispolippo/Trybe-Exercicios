f9c7b53b3785daa818b5c653b793ecd0fa99e92a
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-03 22:07:00 -0300

    adicionado exericcio 1 e 2 dia 11.1

--- 02-Front_End/11-Introduçao_a_React/dia_1-Introduçao_e_Hello_World_no_React/exercicios/exercicios_react/src/App.js
+++ 02-Front_End/11-Introduçao_a_React/dia_1-Introduçao_e_Hello_World_no_React/exercicios/exercicios_react/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+const commitments = ['Estudar', 'Trabalhar', 'Almoçar', 'Pagar boleto', 'Dormir'];
+
+const task = (value) => {
+  return(
+    <li className='listItem'>{value}</li>
+  )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    commitments.map((commitment) => task(commitment))
+  )
 }
 
 export default App;
