39ca56c321765c9f2ffde3403ac655e5fd863400
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-06 20:31:02 -0300

    Alterado para_fixar com os exercicios 5, 6 e 7

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
@@ -4,32 +4,40 @@ import { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
-    this.showMeu = this.showMeu.bind(this);
-    this.showOla = this.showOla.bind(this);
-    this.showQuerido = this.showQuerido.bind(this);
+    this.showButton1 = this.showButton1.bind(this);
+    this.showButton2 = this.showButton2.bind(this);
+    this.showButton3 = this.showButton3.bind(this);
+    this.state = {
+      numeroCliquesBtn1: 0,
+      numeroCliquesBtn2: 0,
+      numeroCliquesBtn3: 0,
+    }
   }
 
-  showOla() {
-    console.log(this);
-    console.log('Olá');
+  showButton1() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliquesBtn1: estadoAnterior.numeroCliquesBtn1 + 1
+    }))
   }
 
-  showMeu() {
-    console.log(this);
-    console.log('meu');
+  showButton2() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliquesBtn2: estadoAnterior.numeroCliquesBtn2 + 1
+    }))
   }
 
-  showQuerido() {
-    console.log(this);
-    console.log('querido');
+  showButton3() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliquesBtn3: estadoAnterior.numeroCliquesBtn3 + 1
+    }))
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.showOla}>Olá</button>
-        <button onClick={this.showMeu}>meu</button>
-        <button onClick={this.showQuerido}>querido</button>
+        <button onClick={this.showButton1}>{this.state.numeroCliquesBtn1}</button>
+        <button onClick={this.showButton2}>{this.state.numeroCliquesBtn2}</button>
+        <button onClick={this.showButton3}>{this.state.numeroCliquesBtn3}</button>
       </div>
     );
   }
