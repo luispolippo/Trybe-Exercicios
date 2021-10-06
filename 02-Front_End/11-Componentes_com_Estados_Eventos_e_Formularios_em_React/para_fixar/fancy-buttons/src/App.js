f904cc1fabae7eb5a70ce136046911495aaf15fe
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-06 19:33:54 -0300

    Implementado exercicio para_fixar fancy-buttons

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function showOla() {
+  console.log('Olá');
+}
+
+function showMeu() {
+  console.log('meu');
+}
+
+function showQuerido() {
+  console.log('querido');
+}
+
+class App extends Component {
+  render() {
+    return <div>
+      <button onClick={showOla}>Olá</button>
+      <button onClick={showMeu}>meu</button>
+      <button onClick={showQuerido}>querido</button>
+    </div>;
+  }
 }
 
 export default App;
