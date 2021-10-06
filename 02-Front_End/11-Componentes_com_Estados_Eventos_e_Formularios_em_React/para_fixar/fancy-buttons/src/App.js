1f722a54e14e973bbe3a5ad7e9cafe21e9bed4fb
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-06 20:08:51 -0300

    Alterado para_fixar com os exercicios 3 e 4

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
@@ -1,25 +1,37 @@
 import './App.css';
 import { Component } from 'react';
 
-function showOla() {
-  console.log('Olá');
-}
-
-function showMeu() {
-  console.log('meu');
-}
-
-function showQuerido() {
-  console.log('querido');
-}
-
 class App extends Component {
+  constructor() {
+    super();
+    this.showMeu = this.showMeu.bind(this);
+    this.showOla = this.showOla.bind(this);
+    this.showQuerido = this.showQuerido.bind(this);
+  }
+
+  showOla() {
+    console.log(this);
+    console.log('Olá');
+  }
+
+  showMeu() {
+    console.log(this);
+    console.log('meu');
+  }
+
+  showQuerido() {
+    console.log(this);
+    console.log('querido');
+  }
+
   render() {
-    return <div>
-      <button onClick={showOla}>Olá</button>
-      <button onClick={showMeu}>meu</button>
-      <button onClick={showQuerido}>querido</button>
-    </div>;
+    return (
+      <div>
+        <button onClick={this.showOla}>Olá</button>
+        <button onClick={this.showMeu}>meu</button>
+        <button onClick={this.showQuerido}>querido</button>
+      </div>
+    );
   }
 }
 
