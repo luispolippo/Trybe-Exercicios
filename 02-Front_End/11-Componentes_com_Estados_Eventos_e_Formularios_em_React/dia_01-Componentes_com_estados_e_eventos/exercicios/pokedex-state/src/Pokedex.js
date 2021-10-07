911c64c243078bf23f5e023d5f8538f3766c2376
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-07 19:35:18 -0300

    Realizado exercicio 2 de pokedex-state dia 11.1

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/dia_01-Componentes_com_estados_e_eventos/exercicios/pokedex-state/src/Pokedex.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/dia_01-Componentes_com_estados_e_eventos/exercicios/pokedex-state/src/Pokedex.js
@@ -2,44 +2,84 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor(props) {
-        super(props);
-        this.nextBtn = this.nextBtn.bind(this);
-        this.previousBtn = this.previousBtn.bind(this);
-    
-        this.state = {
-          currPokemon: 0,
-          pokemonsLength: props.pokemons.length - 1,
-        };
+  constructor(props) {
+    super(props);
+
+    this.nextBtn = this.nextBtn.bind(this);
+    this.previousBtn = this.previousBtn.bind(this);
+    this.filter = this.filter.bind(this);
+    this.btnFire = this.btnFire.bind(this);
+    this.btnPsychic = this.btnPsychic.bind(this);
+
+    this.state = {
+      currPokemon: 0,
+      pokemonType: 'all',
+    };
+  }
+
+  nextBtn(length) {
+    console.log(length)
+    this.setState((prevState) => {
+      if (prevState.currPokemon === length -1) {
+        return { currPokemon: 0 };
       }
-    
-      nextBtn() {
-        this.setState((prevState) => {
-          if (prevState.currPokemon === prevState.pokemonsLength) {
-            return { currPokemon: 0 };
-          }
-          return { currPokemon: prevState.currPokemon + 1 };
-        });
+      return { currPokemon: prevState.currPokemon + 1 };
+    });
+  }
+
+  previousBtn(length) {
+    this.setState((prevState) => {
+      if (prevState.currPokemon === 0) {
+        return { currPokemon: length -1};
       }
-    
-      previousBtn() {
-        this.setState((prevState) => {
-            if (prevState.currPokemon === 0) {
-                return { currPokemon: prevState.pokemonsLength }
-            } 
-            return { currPokemon: prevState.currPokemon - 1 }
-        });
-      }
-    
-      render() {
-        return (
-          <div className='pokedex'>
-            <Pokemon pokemon={this.props.pokemons[this.state.currPokemon]} />
-            <button onClick={this.previousBtn}>Anterior</button>
-            <button onClick={this.nextBtn}>Próximo</button>
-          </div>
-        );
-      }
+      return { currPokemon: prevState.currPokemon - 1 };
+    });
+  }
+
+  filter() {
+    return this.props.pokemons.filter((pokemon) => {
+      if (this.state.pokemonType === 'all') return true;
+      return pokemon.type === this.state.pokemonType;
+    });
+  }
+
+  btnFire() { 
+    this.setState({
+      pokemonType: 'Fire',
+      currPokemon: 0,
+    });
+  }
+
+  btnPsychic() {
+    this.setState({ pokemonType: 'Psychic', currPokemon: 0 });
+  }
+
+  btnAll = () => {
+    this.setState({
+      pokemonType: 'all',
+      pokemonsLength: this.props.pokemons.length - 1,
+    });
+  };
+
+  render() {
+    const filteredPokemons = this.filter();
+    const lengthPokemons = filteredPokemons.length
+    const pokemon = filteredPokemons[this.state.currPokemon];
+    return (
+      <div className='pokedex'>
+        <Pokemon pokemon={pokemon} />
+        <div>
+          <button onClick={() => this.previousBtn(lengthPokemons)}>Anterior</button>
+          <button onClick={() => this.nextBtn(lengthPokemons)}>Próximo</button>
+        </div>
+        <div>
+          <button onClick={this.btnFire}>Fire</button>
+          <button onClick={this.btnPsychic}>Psychic</button>
+          <button onClick={this.btnAll}>All</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
