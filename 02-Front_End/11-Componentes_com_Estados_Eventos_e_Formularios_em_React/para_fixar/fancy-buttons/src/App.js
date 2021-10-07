1d41f073a9e1f414419e2f850f177967e95ececd
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-06 21:09:41 -0300

    Tentativa de fazer exericcio para_fixar 8 e 9

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/para_fixar/fancy-buttons/src/App.js
@@ -11,33 +11,54 @@ class App extends Component {
       numeroCliquesBtn1: 0,
       numeroCliquesBtn2: 0,
       numeroCliquesBtn3: 0,
-    }
+    };
   }
 
   showButton1() {
     this.setState((estadoAnterior, _props) => ({
-      numeroCliquesBtn1: estadoAnterior.numeroCliquesBtn1 + 1
-    }))
+      numeroCliquesBtn1: estadoAnterior.numeroCliquesBtn1 + 1,
+    }));
   }
 
   showButton2() {
     this.setState((estadoAnterior, _props) => ({
-      numeroCliquesBtn2: estadoAnterior.numeroCliquesBtn2 + 1
-    }))
+      numeroCliquesBtn2: estadoAnterior.numeroCliquesBtn2 + 1,
+    }));
   }
 
   showButton3() {
     this.setState((estadoAnterior, _props) => ({
-      numeroCliquesBtn3: estadoAnterior.numeroCliquesBtn3 + 1
-    }))
+      numeroCliquesBtn3: estadoAnterior.numeroCliquesBtn3 + 1,
+    }));
+  }
+
+  setButtonColor(btnNum) {
+    return btnNum % 2 === 0 ? 'green' : 'light-gray';
   }
 
   render() {
+    const { numeroCliquesBtn1, numeroCliquesBtn2, numeroCliquesBtn3 } =
+      this.state;
     return (
       <div>
-        <button onClick={this.showButton1}>{this.state.numeroCliquesBtn1}</button>
-        <button onClick={this.showButton2}>{this.state.numeroCliquesBtn2}</button>
-        <button onClick={this.showButton3}>{this.state.numeroCliquesBtn3}</button>
+        <button
+          onClick={this.showButton1}
+          style={{backgroundColor: this.setButtonColor(numeroCliquesBtn1)}}
+        >
+          {numeroCliquesBtn1}
+        </button>
+        <button
+          onClick={this.showButton2}
+          style={{ backgroundColor: this.setButtonColor(numeroCliquesBtn2) }}
+        >
+          {numeroCliquesBtn2}
+        </button>
+        <button
+          onClick={this.showButton3}
+          style={{ backgroundColor: this.setButtonColor(numeroCliquesBtn3) }}
+        >
+          {numeroCliquesBtn3}
+        </button>
       </div>
     );
   }
