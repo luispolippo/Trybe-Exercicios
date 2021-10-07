7a08704356bfdbd447e0eed258fe5b2f88c39141
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-07 15:28:33 -0300

    Realizado exercicio 1 de pokedex-state dia 11.1

--- 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/dia_01-Componentes_com_estados_e_eventos/exercicios/pokedex-state/src/Pokedex.js
+++ 02-Front_End/11-Componentes_com_Estados_Eventos_e_Formularios_em_React/dia_01-Componentes_com_estados_e_eventos/exercicios/pokedex-state/src/Pokedex.js
@@ -2,13 +2,44 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.nextBtn = this.nextBtn.bind(this);
+        this.previousBtn = this.previousBtn.bind(this);
+    
+        this.state = {
+          currPokemon: 0,
+          pokemonsLength: props.pokemons.length - 1,
+        };
+      }
+    
+      nextBtn() {
+        this.setState((prevState) => {
+          if (prevState.currPokemon === prevState.pokemonsLength) {
+            return { currPokemon: 0 };
+          }
+          return { currPokemon: prevState.currPokemon + 1 };
+        });
+      }
+    
+      previousBtn() {
+        this.setState((prevState) => {
+            if (prevState.currPokemon === 0) {
+                return { currPokemon: prevState.pokemonsLength }
+            } 
+            return { currPokemon: prevState.currPokemon - 1 }
+        });
+      }
+    
+      render() {
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+          <div className='pokedex'>
+            <Pokemon pokemon={this.props.pokemons[this.state.currPokemon]} />
+            <button onClick={this.previousBtn}>Anterior</button>
+            <button onClick={this.nextBtn}>Próximo</button>
+          </div>
         );
-    }
+      }
 }
 
 export default Pokedex;
