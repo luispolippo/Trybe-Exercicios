593d98dda1512b75775eb953872d26eb27b4a0b9
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-17 23:39:26 -0300

    Realizado exercicios exercises-router 1 a 6

---
+++ 02-Front_End/12-Ciclo_de_vida_de_componentes_e_React_Router/dia_02-React_Router/para_fixar/comprehension-exercises-router/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+        </ul>
+        
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users"><Users greetingsMessage="Good Morning"/></Route>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
