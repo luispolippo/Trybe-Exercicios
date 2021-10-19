b289c4f6b5901a5d46aee5617057a48377813261
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-18 22:24:39 -0300

    Exercicio dia 12.2 realizado requisito 5

---
+++ 02-Front_End/12-Ciclo_de_vida_de_componentes_e_React_Router/dia_02-React_Router/exercicios/exercise-pokedex-router/src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          This application simulates a Pokedex wich is a digital encycopedia of Pokemons in Pokemon World.
+        </p>
+        <img src="https://static.wikia.nocookie.net/tkoc/images/5/58/Pok%C3%A9dex.png/revision/latest?cb=20200623144843&path-prefix=pt-br" alt="Pokedex" />
+      </div>
+    );
+  }
+}
+
+export default About;
