e733672a07e10105b2f0cc95060b6c4f3b41003e
Guilherme Polippo <luispolippo@hotmail.com>
2021-10-29 20:47:13 -0300

    adicionado exercise-one e exercise-two

--- 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/exercicios/exercise-todo-list/src/test/exercise-two.test.js
+++ 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/exercicios/exercise-todo-list/src/test/exercise-two.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import Item from '../Item';
 
@@ -8,13 +9,25 @@ afterEach(cleanup);
 describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
-    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
-
+    const { getByText, getByLabelText } = render(<App />) // Caso precise de uma nova query adicione no object destructuring
+    const addButton = getByText(/adicionar/i);
+    const input = getByLabelText(/tarefa/i);
+    listTodo.forEach((item) => {
+      userEvent.type(input, item);
+      userEvent.click(addButton);
+    })
+    listTodo.forEach((item) => {
+      const task = getByText(item);
+      expect(task).toBeInTheDocument();
+    })
   })
 });
 
 describe('Teste do componente Item', () => {
   test('Ao receber uma string como prop ela precisa aparecer na tela', () => {
-
+    const ITEM_PROPS = 'teste'
+    render(<Item content={ ITEM_PROPS }/>)
+    const testProp = screen.getByText(ITEM_PROPS);
+    expect(testProp).toBeInTheDocument();
   })
 })
