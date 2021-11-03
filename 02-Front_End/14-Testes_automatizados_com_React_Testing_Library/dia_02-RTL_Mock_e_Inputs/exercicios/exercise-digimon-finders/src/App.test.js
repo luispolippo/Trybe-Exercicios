34d768c0406ed688954ab04da3ff7d75989a5a1f
Guilherme Polippo <luispolippo@hotmail.com>
2021-11-03 20:21:08 -0300

    finalizado exercicio digimon-finders

--- 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/dia_02-RTL_Mock_e_Inputs/exercicios/exercise-digimon-finders/src/App.test.js
+++ 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/dia_02-RTL_Mock_e_Inputs/exercicios/exercise-digimon-finders/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('Teste da aplicação toda', () => {
   it('renders App', async () => {
+    render(<App />)
+    const inputSearch = screen.getByRole('textbox', { name: /digimon/i });
+    const button = screen.getByRole('button', { name: /search digimon/i });
+    const doASearch = screen.getByRole('heading', { name: /faça uma pesquisa/i });
+    expect(inputSearch).toBeDefined();
+    expect(button).toBeDefined();
+    expect(doASearch).toBeDefined();
 
+    userEvent.type(inputSearch, 'Agumon');
+    userEvent.click(button);
+
+    const digimonName = await screen.findByRole('heading', { name: /Agumon/i });
+    expect(doASearch).not.toBeInTheDocument();
+    expect(digimonName).toBeInTheDocument();
+    
   });
 });
--- 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/dia_02-RTL_Mock_e_Inputs/exercicios/exercise-digimon-finders/src/Digimon.test.js
+++ 02-Front_End/14-Testes_automatizados_com_React_Testing_Library/dia_02-RTL_Mock_e_Inputs/exercicios/exercise-digimon-finders/src/Digimon.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Digimon from './Digimon';
 
+const responseAPI = {
+  name: "Agumon",
+  img: "https://digimon.shadowsmith.com/img/agumon.jpg",
+  level: "Rookie"
+}
 describe('Teste da tela do Digimon', () => {
   it('renders Digimon', async () => {
+    jest.spyOn(global, 'fetch');
 
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI),
+    })
+
+    render(<Digimon digimon={ responseAPI }/>)
+
+    const digimonName = screen.getByTestId('digimonName');
+    const digimonLevel = screen.getByTestId('digimonLevel');
+
+    expect(digimonName).toHaveTextContent('Agumon');
+    expect(digimonLevel).toHaveTextContent('Rookie');
   });
 });
