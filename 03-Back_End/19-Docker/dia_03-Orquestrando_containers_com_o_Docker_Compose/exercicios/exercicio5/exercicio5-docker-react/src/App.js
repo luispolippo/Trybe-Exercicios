d6ea4ea84fef03367e57413b01017ee39b912e8d
Guilherme Polippo <luispolippo@hotmail.com>
2022-01-19 23:31:00 -0300

    adicionado para_fixar e exercicios dia 19.3

---
+++ 03-Back_End/19-Docker/dia_03-Orquestrando_containers_com_o_Docker_Compose/exercicios/exercicio5/exercicio5-docker-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>App React rodando em um container</h1>
+      <p>Container construido com Dockerfile e Compose</p>
+    </div>
+  );
+}
+
+export default App;
