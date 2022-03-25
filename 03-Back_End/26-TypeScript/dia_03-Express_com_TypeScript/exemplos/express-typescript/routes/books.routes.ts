8af4652454914029931dea7cd36f7ae23c68f35c
Guilherme Polippo <luispolippo@hotmail.com>
2022-03-25 19:34:39 -0300

    Adicionado exemplo bloco 26 dia 3

---
+++ 03-Back_End/26-TypeScript/dia_03-Express_com_TypeScript/exemplos/express-typescript/routes/books.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
+import validationBook from '../middlewares/books.middleware';
+
+const router = Router();
+
+const booksController = new BooksController();
+
+const bookSlashId = '/books/:id';
+
+router.get('/books', booksController.getAll);
+router.get(bookSlashId, booksController.getById);
+router.post('/books/', validationBook, booksController.create);
+router.put(bookSlashId, validationBook, booksController.update);
+router.delete(bookSlashId, booksController.remove);
+
+export default router;
